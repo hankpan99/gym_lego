5521b6f3fa3391b2e92eae52271cd184a2b26cbe
hankpan99 <yeyehank@gmail.com>
2023-03-01 02:37:12 +0800

    [dexycb] [v1]: add if __name__ __main__, modify learn()

--- train.py
+++ train.py
@@ -53,50 +53,55 @@ def tensorboard_launcher(directory_path):
     print("Tensorboard session created: " + url)
     webbrowser.open_new(url)
 
-### parse arguments
-args = parse_args()
-num_envs = args.n_envs
+def main():
+    ### parse arguments
+    args = parse_args()
+    num_envs = args.n_envs
 
-### setup device
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    ### setup device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-### Set training step parameters
-pre_grasp_steps = 60
-trail_steps = 135
+    ### Set training step parameters
+    pre_grasp_steps = 60
+    trail_steps = 0
 
-grasp_steps = pre_grasp_steps
-n_steps = grasp_steps  + trail_steps
-total_steps = n_steps * num_envs
+    grasp_steps = pre_grasp_steps
+    n_steps = grasp_steps  + trail_steps
+    total_steps = n_steps * num_envs
 
-# env = make_vec_env(LegoEnv, n_envs=args.n_envs, env_kwargs={"args":args}, vec_env_cls=SubprocVecEnv, vec_env_kwargs=dict(start_method='fork'))
-random_noise_pos = np.random.uniform([-0.02, -0.02, 0],[0.02, 0.02, 0], 3).copy()
-random_noise_qpos = np.random.uniform(-0.05, 0.05, 48).copy()
-env = make_vec_env(DexYCBEnv,
-                   n_envs=num_envs,
-                   env_kwargs={"args":args, 'random_noise':[random_noise_pos, random_noise_qpos], 'max_steps':n_steps},
-                   vec_env_cls=SubprocVecEnv,
-                   vec_env_kwargs=dict(start_method='fork'))
-env.reset()
+    # env = make_vec_env(LegoEnv, n_envs=args.n_envs, env_kwargs={"args":args}, vec_env_cls=SubprocVecEnv, vec_env_kwargs=dict(start_method='fork'))
+    random_noise_pos = np.random.uniform([-0.02, -0.02, 0],[0.02, 0.02, 0], 3).copy()
+    random_noise_qpos = np.random.uniform(-0.05, 0.05, 48).copy()
+    env = make_vec_env(DexYCBEnv,
+                    n_envs=num_envs,
+                    env_kwargs={"args":args, 'random_noise':[random_noise_pos, random_noise_qpos], 'max_steps':n_steps},
+                    vec_env_cls=SubprocVecEnv,
+                    vec_env_kwargs=dict(start_method='fork'))
+    env.reset()
 
-### Set up logging
-models_dir, log_dir = create_ckpt_dirs(args, ['dexycb_env.py', 'dexycb_data_all.pickle', __file__])
-tensorboard_launcher(log_dir)
+    ### Set up logging
+    models_dir, log_dir = create_ckpt_dirs(args, ['dexycb_env.py', 'dexycb_data_all.pickle', __file__])
+    tensorboard_launcher(log_dir)
 
-### Set up RL algorithm
-model = PPO('MlpPolicy',
-            env,
-            n_steps=n_steps,
-            batch_size=4,
-            verbose=1,
-            tensorboard_log=log_dir,
-            device=device)
-iters = 0
+    ### Set up RL algorithm
+    model = PPO('MlpPolicy',
+                env,
+                n_steps=n_steps,
+                batch_size=4,
+                verbose=1,
+                tensorboard_log=log_dir,
+                device=device)
+    iters = 0
+    log_step_cnt = 50
 
-while True:
-    model.learn(total_timesteps=n_steps * num_envs,
-                reset_num_timesteps=False)
-    
-    if iters % 10 == 0:
-        model.save(f"{models_dir}/{iters}")
-    
-    iters += 1
+    while True:
+        model.learn(total_timesteps=total_steps * log_step_cnt,
+                    reset_num_timesteps=False)
+        
+        if iters % log_step_cnt == 0:
+            model.save(f"{models_dir}/{iters}")
+        
+        iters += 1
+
+if __name__ == '__main__':
+    main()
